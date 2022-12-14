8bc3a9c1a8dd87dd657d7fe30276344f3f3f7dde
abkarim <akskarim17@gmail.com>
2022-12-14 14:53:07 +0600

    font changed

--- gui-development/src/pages/auth/Default.jsx
+++ gui-development/src/pages/auth/Default.jsx
@@ -3,10 +3,10 @@ import Link from "../../components/Link";
 export default function Default() {
   return (
     <div className="bg-white p-2 max-w-lg flex-grow rounded-sm shadow-2xl">
-      <h1 className="text-3xl text-gray-900 font-semibold tracking-normal">
+      <h1 className="text-3xl text-gray-900 font-semibold tracking-normal font-lato">
         Select a user role to login
       </h1>
-      <div className="space-y-2">
+      <div className="space-y-2 font-sans mt-2">
         <p>
           Login as <Link to="/login/student">student</Link>
         </p>
