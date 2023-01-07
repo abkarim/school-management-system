603475a9db37791a7f9e18504029a1231e480808
karim <akskarim17@gmail.com>
2023-01-07 17:37:16 +0600

    reset config

--- config.php
+++ config.php
@@ -17,77 +17,77 @@
  * Database host
  * require to connect with database
  */
-define('DATABASE_HOSTNAME', 'localhost');
+define("DATABASE_HOSTNAME", "");
 
 /**
  * Database name
  * require to connect with database
  ** Which database to connect
  */
-define('DATABASE_NAME', 'sc_management');
+define("DATABASE_NAME", "");
 
 /**
  * Database user username
  * require to connect with database
  ** Username to use in connection
  */
-define('DATABASE_USERNAME', 'root');
+define("DATABASE_USERNAME", "");
 
 /**
  * Database user password
  * require to connect with database
  ** Password for the use to use in connection
  */
-define('DATABASE_PASSWORD', 'root');
+define("DATABASE_PASSWORD", "");
 
 /**
  * Database charset
  * require to create table in database
  */
-define('DATABASE_CHARSET', 'utf8');
+define("DATABASE_CHARSET", "utf8");
 
 /**
  * Database table prefix
  * Prepend to table name
  */
-define('TABLE_PREFIX', '');
+define("TABLE_PREFIX", "");
 
 /**
  * Default user image name
  * when user doesn't uploaded an image this image will be in use
  * Image should be placed in public/uploads/image/ directory with this name
  */
-define('DEFAULT_USER_IMAGE_NAME', 'user.png');
+define("DEFAULT_USER_IMAGE_NAME", "user.png");
 
 /**
  * Student id initialization
  * start student id from
  */
-define('STUDENT_ID_START_FROM', '000000000000001');
+define("STUDENT_ID_START_FROM", "000000000000001");
 
 # Maximum image file size in MB
-define('MAX_IMAGE_UPLOAD_SIZE', 10);
+define("MAX_IMAGE_UPLOAD_SIZE", 10);
 
 # Handle installation
-define('APP_INSTALLED', '1');
+define("APP_INSTALLED", "0");
 
 /**
  * Installer IP
  * If same required to create super admin
  */
-define('APP_INSTALLER_IP', '::1');
+define("APP_INSTALLER_IP", "");
 
 /**
  * Environment
  */
-define('APP_ENVIRONMENT', 'development');
+define("APP_ENVIRONMENT", "development");
 
 /**
  * Show pdo error mode - bool
  * turn off in production
  */
-if (APP_ENVIRONMENT === 'development') {
-    define('SHOW_PDO_ERROR', true);
+if (APP_ENVIRONMENT === "development") {
+  define("SHOW_PDO_ERROR", true);
 } else {
-    define('SHOW_PDO_ERROR', false);
+  define("SHOW_PDO_ERROR", false);
 }
