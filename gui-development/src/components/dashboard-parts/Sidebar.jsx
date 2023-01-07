ab65c36603a34b090b6e4ecf1d22880577bc458c
karim <akskarim17@gmail.com>
2023-01-07 17:35:25 +0600

    sidebar updated

--- gui-development/src/components/dashboard-parts/Sidebar.jsx
+++ gui-development/src/components/dashboard-parts/Sidebar.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 
 function MenuItem({ to, children, ...props }) {
   return (
-    <Link className="inline-block" to={to} {...props}>
+    <Link
+      className="inline-block font-sans font-semibold hover:bg-gray-100 px-4 text-left border-b-2 border-black"
+      to={to}
+      {...props}>
       {children}
     </Link>
   );
@@ -12,11 +15,35 @@ function MenuItem({ to, children, ...props }) {
 export default function Sidebar({ ...props }) {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
   return (
-    <aside className={`bg-white relative shadow-xl min-h-screen w-full ${isMenuOpen ? 'max-w-fit' : 'max-w-0' }`} >
-      <div className="overflow-hidden transition">
-          <MenuItem>School</MenuItem>
+    <aside
+      className={`relative shadow-xl min-h-screen w-full transition bg-white ${
+        isMenuOpen ? "max-w-fit" : "max-w-0"
+      }`}>
+      <div className="overflow-hidden transition flex flex-col">
+        <MenuItem to="/school">School</MenuItem>
+        <MenuItem to="#">Users</MenuItem>
+        {/* Users list on drop down */}
+        <div className="flex flex-col ml-2">
+          <MenuItem to="/users/admin">Admin</MenuItem>
+          <MenuItem to="/users/accountant">Accountant</MenuItem>
+          <MenuItem to="/users/student">Student</MenuItem>
+          <MenuItem to="/users/librarian">Librarian</MenuItem>
+          <MenuItem to="/users/parent">Parent</MenuItem>
+          <MenuItem to="/users/teacher">Teacher</MenuItem>
+        </div>
+        <MenuItem to="/class">Class</MenuItem>
+        <MenuItem to="/notice">Notice</MenuItem>
+        <MenuItem to="/exam">Exam</MenuItem>
+        <MenuItem to="/book">Books</MenuItem>
       </div>
-      <span className="inline-block absolute bg-emerald-300 rounded-full text-white font-extrabold font-lato px-2 cursor-pointer select-none" onClick={() => setIsMenuOpen(!isMenuOpen)} > {isMenuOpen? '<' : '>'} </span>
+      <span
+        title={isMenuOpen ? "close menu" : "open menu"}
+        className={`inline-block absolute bg-emerald-300 rounded-full text-white font-extrabold font-lato px-2 cursor-pointer select-none transition top-52 ${
+          isMenuOpen ? "-right-5" : "-left-2 hover:left-1"
+        }`}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        {isMenuOpen ? "<" : ">"}
+      </span>
     </aside>
   );
 }
--- gui-development/src/components/dashboard-parts/Topbar.jsx
+++ gui-development/src/components/dashboard-parts/Topbar.jsx
@@ -1,22 +1,25 @@
 import { useState } from "react";
 import url from "../../util/URL";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-function MenuLink({to,children, ...props}) {
-    return (
-        <Link className="inline-block w-full px-2 cursor-pointer hover:bg-gray-100 font-sans" {...props} to={to}>
-            {children}
-        </Link>
-    )
+function MenuLink({ to, children, ...props }) {
+  return (
+    <Link
+      className="inline-block w-full px-2 cursor-pointer hover:bg-gray-100 font-sans"
+      {...props}
+      to={to}>
+      {children}
+    </Link>
+  );
 }
 
 function Menu() {
   return (
     <div className="absolute right-4 shadow-2xl rounded-sm bg-white">
-      <MenuLink to='/profile'>Profile</MenuLink>
-      <MenuLink to='/settings'>Settings</MenuLink>
+      <MenuLink to="/profile">Profile</MenuLink>
+      <MenuLink to="/settings">Settings</MenuLink>
       <hr />
-      <MenuLink to='/logout'>Logout</MenuLink>
+      <MenuLink to="/logout">Logout</MenuLink>
     </div>
   );
 }
@@ -26,12 +29,11 @@ export default function Topbar({ ...props }) {
   return (
     <header className="relative z-0" {...props}>
       <div className="h-full bg-white shadow-xl p-2 flex justify-between items-center">
-        <h4 className="font-lato text-lg">Title here</h4>
+        <h4 className="font-lato text-lg font-semibold">Title here</h4>
         <div>
           <span
             className="inline-block h-10 bg-white-500 border-2 border-emerald-300 rounded-full p-1 cursor-pointer"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
+            onClick={() => setIsMenuOpen(!isMenuOpen)}>
             <img
               src={`${url.image}/user.png`}
               alt="user"
